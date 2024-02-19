856657387146bf21b7f7f4fd615b8cda1f93c856
brahmkshatriya <69040506+brahmkshatriya@users.noreply.github.com>
2024-02-19 21:04:47 +0530

    add exception handler in RepoComposer

--- plugger/src/main/java/tel/jeelpa/plugger/RepoComposer.kt
+++ plugger/src/main/java/tel/jeelpa/plugger/RepoComposer.kt
@@ -2,11 +2,12 @@ package tel.jeelpa.plugger
 
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.combine
+import java.lang.Exception
 
 class RepoComposer<TPlugin>(private vararg val repos: PluginRepo<TPlugin>) : PluginRepo<TPlugin> {
-    override fun getAllPlugins(): Flow<List<TPlugin>> =
+    override fun getAllPlugins(exceptionHandler: (Exception) -> Unit): Flow<List<TPlugin>> =
         combine(
-            repos.map { it.getAllPlugins() }
+            repos.map { it.getAllPlugins(exceptionHandler) }
         ) { array ->
             array.reduce { a, b -> a + b }
         }
